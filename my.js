7d351c1de9752b459e73436edd2532548a7844f0
ANA5795 <fsx5795@hotmail.com>
2020-08-26 00:18:18 +0800

    xg

--- my.js
+++ my.js
@@ -1,3 +1,17 @@
+function bodyOnload() {
+    var title = document.getElementById("title");
+    title.style.height = window.screen.availHeight + "px";
+    console.log(window.screen.availHeight);
+}
+function selectChanged() {
+    var sel = document.getElementById("theme");
+    var index = sel.selectedIndex;
+    if (index === 0) {
+        sel.className = "selDark";
+    } else {
+        sel.className = "selLight";
+    }
+}
 function searchEnter() {
     var search = document.getElementById("btn").getAttribute("class");
     search = search.replace("search", "search_hover");
