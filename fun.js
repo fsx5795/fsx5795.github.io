2d5329920be57db45c810554122bae8f35a6524a
fsx5795 <fsx5795@hotmail.com>
2023-05-06 17:10:11 +0800

    gitalk

--- fun.js
+++ fun.js
@@ -1,7 +1,7 @@
 var gitalk = new Gitalk({
     clientID: 'Iv1.8e2b7cb55ce2679e',
     clientSecret: 'ad1abaed48da8536fa025c009eddafdacbd5fea6',
-    repo: 'gitalk-comments',
+    repo: 'https://github.com/fsx5795/gitalk-comments',
     owner: 'fsx5795',
     admin:  ['fsx5795'],
     id: location.pathname,
